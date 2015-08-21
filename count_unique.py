b6c3ebeacdb6ec58e4d33610c4e45bd7356a1d2e
Nate Miller <natemiller77@mail.com>
2015-08-21 13:23:13 -0700

    Modified to take a long string as input and split into list of strings for analysis

--- count_unique.py
+++ count_unique.py
@@ -1,6 +1,6 @@
 def count_unique(input):
 	dict = {} #create a dictionary to hold values and keys
-	
+	input = input.split()
 	for i in input:		#iterate through the input list
 		if i in dict:		#test if each element of the list is a key in the dictionary
 			dict[i] += 1	#if so, increase the value of that key by one
@@ -11,17 +11,15 @@ def count_unique(input):
 #each tuple for sorting (representing the count), and sorting from highest to
 #lowest (reverse = True)
 
-	print sorted(dict.items(), key = lambda dict: dict[1], reverse=True)
-
+	sorted_list = sorted(dict.items(), key = lambda dict: dict[1], reverse=True)
+	#sorted_list = sorted(dict.items(), key = lambda (x,y): (x.lower(), y))
+	print sorted_list
 #define a main function which will call the count_unique function above and 
 #apply it to a vector of strings
 def main():
-	vec = ['a','b','a','b','b','c','c','q','q']
+	vec = ("Hi there how are you doing today? I'm doing great thanks for asking. How about you?")
 	count_unique(vec)
-	vec2 = ['hat', 'hat','shoe','shoe','shoe', 'cane','smile']
-	count_unique(vec2)
-	vec3 = ['hello','how','are','you','doing','today', 'today', 'I','am','doing','well','how','are','you','doing']
-	count_unique(vec3)
+	
 
 # boilerplate to run the main function with no command-line inputs
 if __name__ == '__main__':
